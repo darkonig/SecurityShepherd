c825c7e0cd6333e874ec25be25202caad312c348
w370 <w370@10.20.60.75>
2018-10-25 15:15:34 -0300

    update

--- owaspSecurityShepherd/src/main/java/servlets/admin/userManagement/AddPlayer.java
+++ owaspSecurityShepherd/src/main/java/servlets/admin/userManagement/AddPlayer.java
@@ -54,145 +54,144 @@ public class AddPlayer extends HttpServlet
 	 * @param csrfToken
 	 */
 	public void doPost (HttpServletRequest request, HttpServletResponse response) 
-	throws ServletException, IOException
 	{
-		//Setting IpAddress To Log and taking header for original IP if forwarded from proxy
-		ShepherdLogManager.setRequestIp(request.getRemoteAddr(), request.getHeader("X-Forwarded-For"));
-		log.debug("*** servlets.Admin.addPlayer ***");
-		PrintWriter out = response.getWriter();  
-		out.print(getServletInfo());
-		HttpSession ses = request.getSession(true);
-		Cookie tokenCookie = Validate.getToken(request.getCookies());
-		Object tokenParmeter = request.getParameter("csrfToken");
-		if(Validate.validateAdminSession(ses, tokenCookie, tokenParmeter))
-		{
-			ShepherdLogManager.setRequestIp(request.getRemoteAddr(), request.getHeader("X-Forwarded-For"), ses.getAttribute("userName").toString());
-			if(Validate.validateTokens(tokenCookie, tokenParmeter))
+		PrintWriter out = null;
+		try {
+			//Setting IpAddress To Log and taking header for original IP if forwarded from proxy
+			ShepherdLogManager.setRequestIp(request.getRemoteAddr(), request.getHeader("X-Forwarded-For"));
+			log.debug("*** servlets.Admin.addPlayer ***");
+			out = response.getWriter();  
+			out.print(getServletInfo());
+			HttpSession ses = request.getSession(true);
+			Cookie tokenCookie = Validate.getToken(request.getCookies());
+			Object tokenParmeter = request.getParameter("csrfToken");
+			if(Validate.validateAdminSession(ses, tokenCookie, tokenParmeter))
 			{
-				boolean notNull = false;
-				boolean notEmpty = false;
-				boolean validPasswords = false;
-				boolean validAddress = false;
-				boolean userValidate = false;
-				String[] classInfo = new String[2];
-				try
+				ShepherdLogManager.setRequestIp(request.getRemoteAddr(), request.getHeader("X-Forwarded-For"), ses.getAttribute("userName").toString());
+				if(Validate.validateTokens(tokenCookie, tokenParmeter))
 				{
-					log.debug("Getting ApplicationRoot");
-					String ApplicationRoot = getServletContext().getRealPath("");					log.debug("Servlet root = " + ApplicationRoot );
-					
-					log.debug("Getting Parameters");
-					String classId = StringEscapeUtils.escapeHtml4(request.getParameter("classId"));
-					log.debug("classId = " + classId);
-					String userName = StringEscapeUtils.escapeHtml4(request.getParameter("userName"));
-					log.debug("userName = " + userName);
-					String passWord = StringEscapeUtils.escapeHtml4(request.getParameter("passWord"));
-					log.debug("passWord retrieved");
-					String passWordConfirm = StringEscapeUtils.escapeHtml4(request.getParameter("passWordConfirm"));
-					log.debug("passWordConfirm retrieved");
-					String userAddress = Validate.validateParameter(request.getParameter("userAddress"), 128);
-					log.debug("userAddress = " + userAddress);
-					String userAddressCnf = Validate.validateParameter(request.getParameter("userAddressCnf"), 128);
-					log.debug("userAddressCnf = " + userAddressCnf);
-					
-					//Validation
-					notNull = (userName != null && passWord != null);
-					log.debug("Ensuring strings are not empty");
-					notEmpty = (!userName.isEmpty() && !passWord.isEmpty());
-					log.debug("Validating passwords");
-					validPasswords = passWord.compareTo(passWordConfirm) == 0; // 0 returned if the same
-					log.debug("Validating addresses");
-					validAddress = userAddress.compareTo(userAddressCnf) == 0;
-					if(userAddress.isEmpty())
-						validAddress = true; //Null Addresses are ok
-					else
-						validAddress = (Validate.isValidEmailAddress(userAddress) && validAddress); //Malformed Addresses are Not ok
-					
-					boolean basicValidation = validPasswords && validAddress && notNull && notEmpty;
-					if(basicValidation)
-						userValidate = (Validate.isValidUser(userName, passWord, userAddress));
-					if(userValidate)
-					{
-						classInfo = Getter.getClassInfo(ApplicationRoot, classId);
-					}
-					if(basicValidation && userValidate && classInfo != null)
-					{
-						String reponseMessage = new String();
-						//Data is good, Add user
-						log.debug("Adding player to class " + classInfo[0] + " " + classInfo[1]);
-						if(Setter.userCreate(ApplicationRoot, classId, userName, passWord, "player", userAddress, true))
+					boolean notNull = false;
+					boolean notEmpty = false;
+					boolean validPasswords = false;
+					boolean validAddress = false;
+					boolean userValidate = false;
+					String[] classInfo = new String[2];
+						log.debug("Getting ApplicationRoot");
+						String ApplicationRoot = getServletContext().getRealPath("");					log.debug("Servlet root = " + ApplicationRoot );
+						
+						log.debug("Getting Parameters");
+						String classId = StringEscapeUtils.escapeHtml4(request.getParameter("classId"));
+						log.debug("classId = " + classId);
+						String userName = StringEscapeUtils.escapeHtml4(request.getParameter("userName"));
+						log.debug("userName = " + userName);
+						String passWord = StringEscapeUtils.escapeHtml4(request.getParameter("passWord"));
+						log.debug("passWord retrieved");
+						String passWordConfirm = StringEscapeUtils.escapeHtml4(request.getParameter("passWordConfirm"));
+						log.debug("passWordConfirm retrieved");
+						String userAddress = Validate.validateParameter(request.getParameter("userAddress"), 128);
+						log.debug("userAddress = " + userAddress);
+						String userAddressCnf = Validate.validateParameter(request.getParameter("userAddressCnf"), 128);
+						log.debug("userAddressCnf = " + userAddressCnf);
+						
+						//Validation
+						notNull = (userName != null && passWord != null);
+						log.debug("Ensuring strings are not empty");
+						notEmpty = (!userName.isEmpty() && !passWord.isEmpty());
+						log.debug("Validating passwords");
+						validPasswords = passWord.compareTo(passWordConfirm) == 0; // 0 returned if the same
+						log.debug("Validating addresses");
+						validAddress = userAddress.compareTo(userAddressCnf) == 0;
+						if(userAddress.isEmpty())
+							validAddress = true; //Null Addresses are ok
+						else
+							validAddress = (Validate.isValidEmailAddress(userAddress) && validAddress); //Malformed Addresses are Not ok
+						
+						boolean basicValidation = validPasswords && validAddress && notNull && notEmpty;
+						if(basicValidation)
+							userValidate = (Validate.isValidUser(userName, passWord, userAddress));
+						if(userValidate)
 						{
-							if(userAddress.isEmpty())
-								userAddress = "blank"; //For Output Message
-							reponseMessage = "<p>" +
-								"Player <a>" + Encode.forHtml(userName) +"</a> added to <a>" + Encode.forHtml(classInfo[0] + " " + classInfo[1]) + "</a> created successfully." +
-								"</p>";
+							classInfo = Getter.getClassInfo(ApplicationRoot, classId);
+						}
+						if(basicValidation && userValidate && classInfo != null)
+						{
+							String reponseMessage = new String();
+							//Data is good, Add user
+							log.debug("Adding player to class " + classInfo[0] + " " + classInfo[1]);
+							if(Setter.userCreate(ApplicationRoot, classId, userName, passWord, "player", userAddress, true))
+							{
+								if(userAddress.isEmpty())
+									userAddress = "blank"; //For Output Message
+								reponseMessage = "<p>" +
+									"Player <a>" + Encode.forHtml(userName) +"</a> added to <a>" + Encode.forHtml(classInfo[0] + " " + classInfo[1]) + "</a> created successfully." +
+									"</p>";
+							}
+							else
+							{
+								reponseMessage = "<div id='error' class='informationBox'><p colour='red'><strong>" +
+										Encode.forHtml(userName) + " was not added to the system due to an error." +
+								"</strong></p></div>";
+							}
+							out.print(reponseMessage);
 						}
 						else
 						{
-							reponseMessage = "<div id='error' class='informationBox'><p colour='red'><strong>" +
-									Encode.forHtml(userName) + " was not added to the system due to an error." +
-							"</strong></p></div>";
+							//Validation Error Responses
+							String errorMessage = "An Error Occurred: ";
+							if(!notNull || !notEmpty)
+							{
+								log.error("Null values detected");
+								errorMessage += "Invalid Request. Please try again";
+							}
+							else if(!validPasswords)
+							{
+								log.error("Passwords did not match");
+								errorMessage += "Password fields did not match";
+							}
+							else if(!validAddress)
+							{
+								log.error("Invalid Addresses Detected");
+								errorMessage += "Invalid Addresses Detected";
+							}
+							else if(!userValidate)
+							{
+								log.error("JavaScript validation bypassed");
+								errorMessage += "Invalid Request. Please try again";
+							}
+							else
+							{
+								log.debug("Class Not Found");
+								errorMessage += "Class not found.";
+							}
+							out.print("<div id='error' class='informationBox'><p colour='red'><strong>" +
+									Encode.forHtml(errorMessage) +
+									"</strong></p></div>");
 						}
-						out.print(reponseMessage);
-					}
-					else
-					{
-						//Validation Error Responses
-						String errorMessage = "An Error Occurred: ";
-						if(!notNull || !notEmpty)
-						{
-							log.error("Null values detected");
-							errorMessage += "Invalid Request. Please try again";
-						}
-						else if(!validPasswords)
-						{
-							log.error("Passwords did not match");
-							errorMessage += "Password fields did not match";
-						}
-						else if(!validAddress)
-						{
-							log.error("Invalid Addresses Detected");
-							errorMessage += "Invalid Addresses Detected";
-						}
-						else if(!userValidate)
-						{
-							log.error("JavaScript validation bypassed");
-							errorMessage += "Invalid Request. Please try again";
-						}
-						else
-						{
-							log.debug("Class Not Found");
-							errorMessage += "Class not found.";
-						}
-						out.print("<div id='error' class='informationBox'><p colour='red'><strong>" +
-								Encode.forHtml(errorMessage) +
-								"</strong></p></div>");
-					}
 				}
-				catch (Exception e)
-				{		
-					e.printStackTrace();
+				else
+				{
+					log.debug("CSRF tokens did not match");
 					out.print("<div id='error' class='informationBox'><p colour='red'><strong>" +
-							"An error Occurred! Please try again." +
-							"</strong></p></div>");
+							"An error Occurred! CSRF Tokens did not match." +
+							"</strong><p></div>");
 				}
 			}
 			else
 			{
-				log.debug("CSRF tokens did not match");
+				out.print(
+						"<div id='error' class='informationBox'><p colour='red'><strong>" +
+						"An error Occurred! Please try non administrator functions!" +
+						"</strong></p></div>");
+			}
+			log.debug("*** addPlayer END ***");
+		} catch (Exception e) {
+			log.error("Error " + e.getMessage());
+			if (out != null) {
 				out.print("<div id='error' class='informationBox'><p colour='red'><strong>" +
-						"An error Occurred! CSRF Tokens did not match." +
-						"</strong><p></div>");
+						"An error Occurred! Please try again." +
+						"</strong></p></div>");
 			}
 		}
-		else
-		{
-			out.print(
-					"<div id='error' class='informationBox'><p colour='red'><strong>" +
-					"An error Occurred! Please try non administrator functions!" +
-					"</strong></p></div>");
-		}
-		log.debug("*** addPlayer END ***");
 	}
 
 }
